057686055f9a1d6bae5096bb1496cf3c64fe0f40
Vikas Kumar Gupta <vikasgup074@gmail.com>
2025-03-28 12:18:51 +0530

    Fixing Issues

--- src/main/java/com/ecommerce/project/SbEcomApplication.java
+++ src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -16,6 +16,21 @@ public class  SbEcomApplication {
 		System.setProperty("DEV_JWT_SECRET", Objects.requireNonNull(dotenv.get("DEV_JWT_SECRET")));
 		System.setProperty("DEV_JWT_EXPIRATION_MS", Objects.requireNonNull(dotenv.get("DEV_JWT_EXPIRATION_MS")));
 		System.setProperty("DEV_JWT_COOKIE_NAME", Objects.requireNonNull(dotenv.get("DEV_JWT_COOKIE_NAME")));
+
+		System.setProperty("PROD_DB_URL", Objects.requireNonNull(dotenv.get("PROD_DB_URL")));
+		System.setProperty("PROD_DB_USERNAME", Objects.requireNonNull(dotenv.get("PROD_DB_USERNAME")));
+		System.setProperty("PROD_DB_PASSWORD", Objects.requireNonNull(dotenv.get("PROD_DB_PASSWORD")));
+		System.setProperty("PROD_JWT_SECRET", Objects.requireNonNull(dotenv.get("PROD_JWT_SECRET")));
+		System.setProperty("PROD_JWT_EXPIRATION_MS", Objects.requireNonNull(dotenv.get("PROD_JWT_EXPIRATION_MS")));
+		System.setProperty("PROD_JWT_COOKIE_NAME", Objects.requireNonNull(dotenv.get("PROD_JWT_COOKIE_NAME")));
+
+		System.setProperty("QA_DB_URL", Objects.requireNonNull(dotenv.get("QA_DB_URL")));
+		System.setProperty("QA_DB_USERNAME", Objects.requireNonNull(dotenv.get("QA_DB_USERNAME")));
+		System.setProperty("QA_DB_PASSWORD", Objects.requireNonNull(dotenv.get("QA_DB_PASSWORD")));
+		System.setProperty("QA_JWT_SECRET", Objects.requireNonNull(dotenv.get("QA_JWT_SECRET")));
+		System.setProperty("QA_JWT_EXPIRATION_MS", Objects.requireNonNull(dotenv.get("QA_JWT_EXPIRATION_MS")));
+		System.setProperty("QA_JWT_COOKIE_NAME", Objects.requireNonNull(dotenv.get("QA_JWT_COOKIE_NAME")));
+
 		SpringApplication.run(SbEcomApplication.class, args);
 	}
 }
